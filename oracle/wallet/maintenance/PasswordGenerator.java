85590d0a0c7807ca1c3bd1525c9ac32ee13d481a
Richard <rngobile@gmail.com>
2018-03-31 19:36:04 -0400

    Created PasswordGenerator

---
+++ oracle/wallet/maintenance/PasswordGenerator.java
@@ -0,0 +1,30 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static SecureRandom random = new SecureRandom();
+
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUMBER = "1234567890";
+    private static final String SPECIAL = "^!%=?{[]}+~#_.:,<>|\\";
+    private static final String DICTIONARY = UPPERCASE + LOWERCASE + NUMBER + SPECIAL;
+
+    public String generatePassword(int length){
+        String password = "";
+        char oldChar = '\0';
+        char newChar = '\0';
+
+        for (int i = 0; i < length; i++){
+            int index = random.nextInt(DICTIONARY.length());
+            newChar = DICTIONARY.charAt(index);
+            if ((i == 0) || (newChar != oldChar)) {
+                password += newChar;
+                oldChar = newChar;
+            } else {
+                i--;
+            }
+        }
+
+        return password;
+    }
+}
