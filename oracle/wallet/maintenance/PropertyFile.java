d43fca2df9e877ecfa11fca15027d5f800f85e04
Richard <rngobile@gmail.com>
2018-04-02 10:16:25 -0400

    added setProperty and closeFile

--- oracle/wallet/maintenance/PropertyFile.java
+++ oracle/wallet/maintenance/PropertyFile.java
@@ -3,14 +3,20 @@ package oracle.wallet.maintenance;
 import java.util.Properties;
 import java.io.InputStream;
 import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
 
 public class PropertyFile {
     private Properties prop = new Properties();
+    OutputStream out = null;
+    InputStream in = null;
     
     public PropertyFile(String file){
         try {
-            InputStream is = new FileInputStream(file);
-            this.prop.load(is);
+            this.in = new FileInputStream(file);
+            this.prop.load(this.in);
+            this.in.close();
+            this.out = new FileOutputStream(file);
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(0);
@@ -19,5 +25,18 @@ public class PropertyFile {
     
     public String getProperty(String propertyName){
         return this.prop.getProperty(propertyName);
+    }
+
+    public void setProperty(String propertyName, String propertyValue){
+        this.prop.setProperty(propertyName, propertyValue);
+    }
+
+    public void closeFile(){
+        try{
+            this.prop.store(out, null);
+            this.out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
